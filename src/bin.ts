607fe3d48f5de6e8614b8aa0feaa04b1bfce5aa7
0918nobita <nobita.0918@gmail.com>
2021-01-04 16:46:14 +0900

    :sparkles: ファイル名を指定して ActionCreator 定義ファイルを生成できるようにした

--- src/bin.ts
+++ src/bin.ts
@@ -2,11 +2,13 @@
 
 import chalk from 'chalk';
 
+import { bootstrap } from './lib';
+
 const args = process.argv.splice(2);
 
 if (args.length === 0) {
   console.log(`${chalk.green.bold('redux-acc')} - Redux Action Creator Creator`);
   console.log('version 0.1.0');
+} else {
+  bootstrap(args);
 }
-
-console.log(args);
--- src/lib.ts
+++ src/lib.ts
@@ -1,9 +1,6 @@
 import * as fs from 'fs';
+import * as path from 'path';
 import * as ts from 'typescript';
-
-const program = ts.createProgram(['example/actions.ts'], {});
-const source = program.getSourceFile('example/actions.ts')!;
-const typeChecker = program.getTypeChecker();
 
 interface ActionTypeDefinition {
   name: string;
@@ -11,123 +8,136 @@ interface ActionTypeDefinition {
   hasPayload: boolean;
 }
 
-const tryGetActionTypeDef = (node: ts.InterfaceDeclaration): ActionTypeDefinition | null => {
-  const name = node.name.getText();
-  const res = name.match(/(.*)Action$/);
+export const bootstrap = (files: string[]): void => {
+  for (const file of files) {
+    const program = ts.createProgram([file], {});
+    const source = program.getSourceFile(file)!;
+    const typeChecker = program.getTypeChecker();
 
-  let type: string | null = null;
-  let hasPayload = false;
+    const tryGetActionTypeDef = (node: ts.InterfaceDeclaration): ActionTypeDefinition | null => {
+      const name = node.name.getText();
+      const res = name.match(/(.*)Action$/);
 
-  if (!res || res.length === 0) return null;
+      let type: string | null = null;
+      let hasPayload = false;
 
-  for (const member of node.members) {
-    const t = typeChecker.getTypeAtLocation(member);
+      if (!res || res.length === 0) return null;
 
-    if (!member.name) continue;
-    if (!ts.isIdentifier(member.name)) continue;
-    const text = member.name.getText();
+      for (const member of node.members) {
+        const t = typeChecker.getTypeAtLocation(member);
 
-    if (text === 'type' && t.isStringLiteral()) {
-      type = t.value;
-      continue;
+        if (!member.name) continue;
+        if (!ts.isIdentifier(member.name)) continue;
+        const text = member.name.getText();
+
+        if (text === 'type' && t.isStringLiteral()) {
+          type = t.value;
+          continue;
+        }
+
+        if (text === 'payload') {
+          hasPayload = true;
+        }
+      }
+
+      if (!type) return null;
+      return { name: res[1]!, type, hasPayload };
+    };
+
+    const actionTypeDefs: ActionTypeDefinition[] = []
+
+    ts.forEachChild(source, (node) => {
+      if (ts.isInterfaceDeclaration(node)) {
+        const actionTypeDef = tryGetActionTypeDef(node);
+        if (actionTypeDef) actionTypeDefs.push(actionTypeDef);
+      }
+    });
+
+    const importElements: ts.ImportSpecifier[] = [];
+    const funcDecls: ts.FunctionDeclaration[] = [];
+
+    for (const actionTypeDef of actionTypeDefs) {
+      const actionTypeIdent = ts.factory.createIdentifier(`${actionTypeDef.name}Action`);
+
+      importElements.push(ts.factory.createImportSpecifier(
+        /* propertyName */ undefined,
+        /* name */ actionTypeIdent
+      ));
+
+      const properties: ts.ObjectLiteralElementLike[] = [
+        ts.factory.createPropertyAssignment(
+          ts.factory.createIdentifier('type'),
+          ts.factory.createStringLiteral(actionTypeDef.type)
+        ),
+      ];
+      if (actionTypeDef.hasPayload) {
+        properties.push(
+          ts.factory.createShorthandPropertyAssignment(
+            ts.factory.createIdentifier('payload')
+          )
+        );
+      }
+      const retStmt = ts.factory.createReturnStatement(
+        ts.factory.createObjectLiteralExpression(properties)
+      );
+
+      const toLowerCamelCase = (str: string): string => str.charAt(0).toLowerCase() + str.slice(1);
+
+      const funcDecl = ts.factory.createFunctionDeclaration(
+        /* decorators */ undefined,
+        /* modifiers */ [ts.factory.createToken(ts.SyntaxKind.ExportKeyword)],
+        /* asteriskToken */ undefined,
+        /* name */ toLowerCamelCase(actionTypeDef.name),
+        /* typeParameters */ undefined,
+        /* parameters */
+          actionTypeDef.hasPayload ?
+            [
+              ts.factory.createParameterDeclaration(
+                /* decorators */ undefined,
+                /* modifiers */ undefined,
+                /* dotDotDotToken */ undefined,
+                ts.factory.createIdentifier('payload'),
+                /* questionToken */ undefined,
+                ts.factory.createIndexedAccessTypeNode(
+                  ts.factory.createTypeReferenceNode(actionTypeIdent),
+                  ts.factory.createLiteralTypeNode(ts.factory.createStringLiteral('payload'))
+                )
+              ),
+            ]
+            : [],
+        /* returnType */ ts.factory.createTypeReferenceNode(actionTypeIdent),
+        ts.factory.createBlock([retStmt], /* multiline */ true)
+      );
+
+      funcDecls.push(funcDecl);
     }
 
-    if (text === 'payload') {
-      hasPayload = true;
-    }
-  }
+    const dirname = path.dirname(file);
+    const basename = path.basename(file, '.ts');
 
-  if (!type) return null;
-  return { name: res[1]!, type, hasPayload };
-};
-
-const actionTypeDefs: ActionTypeDefinition[] = []
-
-ts.forEachChild(source, (node) => {
-  if (ts.isInterfaceDeclaration(node)) {
-    const actionTypeDef = tryGetActionTypeDef(node);
-    if (actionTypeDef) actionTypeDefs.push(actionTypeDef);
-  }
-});
-
-const importElements: ts.ImportSpecifier[] = [];
-const funcDecls: ts.FunctionDeclaration[] = [];
-
-for (const actionTypeDef of actionTypeDefs) {
-  const actionTypeIdent = ts.factory.createIdentifier(`${actionTypeDef.name}Action`);
-
-  importElements.push(ts.factory.createImportSpecifier(
-    /* propertyName */ undefined,
-    /* name */ actionTypeIdent
-  ));
-
-  const properties: ts.ObjectLiteralElementLike[] = [
-    ts.factory.createPropertyAssignment(
-      ts.factory.createIdentifier('type'),
-      ts.factory.createStringLiteral(actionTypeDef.type)
-    ),
-  ];
-  if (actionTypeDef.hasPayload) {
-    properties.push(
-      ts.factory.createShorthandPropertyAssignment(
-        ts.factory.createIdentifier('payload')
-      )
+    const importDecl = ts.factory.createImportDeclaration(
+      /* decorators */ undefined,
+      /* modifiers */ undefined,
+      ts.factory.createImportClause(
+        /* isTypeOnly */ true,
+        /* name */ undefined,
+        /* namedBindings */ ts.factory.createNamedImports(importElements)
+      ),
+      ts.factory.createStringLiteral(`./${basename}`)
     );
+
+    const nodeArray = ts.factory.createNodeArray([importDecl, ...funcDecls]);
+
+    const outFile = ts.createSourceFile(
+      'out.ts',
+      /* sourceText */ '',
+      ts.ScriptTarget.Latest,
+      /* setParentNodes */ false,
+      ts.ScriptKind.TS
+    );
+    const printer = ts.createPrinter({ newLine: ts.NewLineKind.LineFeed });
+    const result = printer.printList(ts.ListFormat.MultiLine, nodeArray, outFile);
+
+    fs.writeFileSync(`${dirname}${path.sep}${basename}.creators.ts`, result);
   }
-  const retStmt = ts.factory.createReturnStatement(
-    ts.factory.createObjectLiteralExpression(properties)
-  );
-
-  const toLowerCamelCase = (str: string): string => str.charAt(0).toLowerCase() + str.slice(1);
-
-  const funcDecl = ts.factory.createFunctionDeclaration(
-    /* decorators */ undefined,
-    /* modifiers */ [ts.factory.createToken(ts.SyntaxKind.ExportKeyword)],
-    /* asteriskToken */ undefined,
-    /* name */ toLowerCamelCase(actionTypeDef.name),
-    /* typeParameters */ undefined,
-    /* parameters */
-      actionTypeDef.hasPayload ?
-        [
-          ts.factory.createParameterDeclaration(
-            /* decorators */ undefined,
-            /* modifiers */ undefined,
-            /* dotDotDotToken */ undefined,
-            ts.factory.createIdentifier('payload'),
-            /* questionToken */ undefined,
-            ts.factory.createIndexedAccessTypeNode(
-              ts.factory.createTypeReferenceNode(actionTypeIdent),
-              ts.factory.createLiteralTypeNode(ts.factory.createStringLiteral('payload'))
-            )
-          ),
-        ]
-        : [],
-    /* returnType */ ts.factory.createTypeReferenceNode(actionTypeIdent),
-    ts.factory.createBlock([retStmt], /* multiline */ true)
-  );
-
-  funcDecls.push(funcDecl);
-}
-
-const importDecl = ts.factory.createImportDeclaration(
-  /* decorators */ undefined,
-  /* modifiers */ undefined,
-  ts.factory.createImportClause(
-    /* isTypeOnly */ true,
-    /* name */ undefined,
-    /* namedBindings */ ts.factory.createNamedImports(importElements)
-  ),
-  ts.factory.createStringLiteral('./example/actions')
-);
-const nodeArray = ts.factory.createNodeArray([importDecl, ...funcDecls]);
-
-const outFile = ts.createSourceFile(
-   'out.ts',
-   /* sourceText */ '',
-  ts.ScriptTarget.Latest,
-  /* setParentNodes */ false,
-  ts.ScriptKind.TS
-);
-const printer = ts.createPrinter({ newLine: ts.NewLineKind.LineFeed });
-const result = printer.printList(ts.ListFormat.MultiLine, nodeArray, outFile);
-fs.writeFileSync('out.ts', result);
+};
